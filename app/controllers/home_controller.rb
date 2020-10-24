cdf43d1a5807eb7f421f0c3e82c6e50acda82a40
Romi Maccarini <romi@clay.cl>
2020-10-24 18:27:41 -0300

    coffee dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,61 @@
 class HomeController < ApplicationController
   def dashboard
+
+    # Ventas mensuales (12 meses)
+    @sales_12_months = Sale.where(created_at: 11.months.ago..Date.today)
+                        .group("strftime('%m-%Y', created_at)")
+                        .order(created_at: :asc)
+                        .sum(:amount)
+
+    # Cantidad de ventas mensuales (12 meses)                    
+    @quantity_12_months = Sale.where(created_at: 11.months.ago..Date.today)
+                        .group("strftime('%m-%Y', created_at)")
+                        .order(created_at: :asc)
+                        .count
+
+    # Promedio de venta (12 meses)
+    @average_12_months = Sale.where(created_at: 11.months.ago..Date.today)
+                        .group("strftime('%m-%Y', created_at)")
+                        .order(created_at: :asc)
+                        .average(:amount)
+
+    # Gráficos de torta que muestren la cantidad de ventas según el origen del café
+    # Los últimos 12 meses.
+    @pie_chart_12_months = Sale.where(created_at: 11.months.ago..Date.today)
+                        .group(:origin)
+                        .limit(12)
+                        .count
+    # Los últimos 6 meses.
+    @pie_chart_6_months = Sale.where(created_at: 5.months.ago..Date.today)
+                        .group(:origin)
+                        .count
+    # Los últimos 3 meses.
+    @pie_chart_3_months = Sale.where(created_at: 2.months.ago..Date.today)
+                        .group(:origin)
+                        .count
+    # El último mes
+    @pie_chart_last_months = Sale.where(created_at: 30.days.ago..Date.today)
+                        .group(:origin)
+                        .count
+
+    # Gráficos de torta que muestren la cantidad de ventas según el nombre de la mezcla del café en:
+    # Los últimos 12 meses.
+    @pie_blend_name_12_months = Sale.where(created_at: 11.months.ago..Date.today)
+                        .order(count: :desc)
+                        .group(:blend_name)
+                        .limit(12)
+                        .count
+    # Los últimos 6 meses.
+    @pie_blend_name_6_months = Sale.where(created_at: 5.months.ago..Date.today)
+                        .group(:blend_name)
+                        .count
+    # Los últimos 3 meses.
+    @pie_blend_name_3_months = Sale.where(created_at: 2.months.ago..Date.today)
+                        .group(:blend_name)
+                        .count
+    # El último mes
+    @pie_blend_name_last_months = Sale.where(created_at: 30.days.ago..Date.today)
+                        .group(:blend_name)
+                        .count
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+    Sale.create(
+        amount: rand(1990..5490),
+        origin: Faker::Coffee.origin,
+        blend_name: Faker::Coffee.blend_name,
+        # created_at: Faker::Date.between(from: '2017-01-01', to: Date.today)
+        created_at: Faker::Date.forward(days: 23)
+    )
+end
